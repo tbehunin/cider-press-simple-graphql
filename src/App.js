70351d98e60568d1c51430140ed62566d507cafa
todd.behunin@nike.com <todd.behunin@nike.com>
2018-10-18 10:13:59 -0700

    create view menu structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RestView from './RestView';
+import JavascriptView from './JavascriptView';
+import ApolloView from './ApolloView';
 
+const views = ['rest', 'javascript graphql client', 'apollo client']
 class App extends Component {
+  state = {
+    currentView: 'rest',
+  };
+  handleMenuClick = view => {
+    this.setState({ currentView: view });
+  }
   render() {
+    const { currentView } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ul className="menu-links">
+            {views.map(view => (
+              <li
+                className={view === currentView ? 'selected' : ''}
+                key={view}
+                onClick={this.handleMenuClick.bind(this, view)}
+              >
+                {view}
+              </li>
+            ))}
+          </ul>
         </header>
+        {currentView === 'rest' ? <RestView /> :
+          currentView === 'javascript graphql client' ? <JavascriptView /> : <ApolloView />
+        }
       </div>
     );
   }
---
+++ src/RestView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class RestView extends React.Component {
+    render() {
+        return (
+            <div>
+                RestView!
+            </div>
+        );
+    }
+}
