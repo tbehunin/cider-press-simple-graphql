6ddecce618c2e8c0a95a300777775ae03ad02725
todd.behunin@nike.com <todd.behunin@nike.com>
2018-10-19 13:04:08 -0700

    added bootstrap

--- src/RestView.js
+++ src/RestView.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export default class RestView extends React.Component {
     render() {
         return (
             <div>
                 RestView!
+                <Button bsStyle="info" bsSize="large">Hello!</Button>
             </div>
         );
     }
